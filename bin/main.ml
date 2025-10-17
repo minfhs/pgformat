b358849486a1d54b73fec950ab129ec4457088f5
Alexander Brevig <alexander.brevig@hey.com>
2025-10-18 00:44:38 +0200

    fix: enable config from cli

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,36 @@
 open Pgformat
 open Core
 
-(* let () = Pgformatter.format_file "./fixture/complex.sql" *)
-
-let format_content = function
-  | None | Some "-" -> Pgformatter.format_stdio ()
-  | Some file -> Pgformatter.format_file file
+let format_content ~indent_size ~max_line_length = function
+  | None | Some "-" -> 
+      (* Format from stdin *)
+      let lexbuf = Lexing.from_channel In_channel.stdin in
+      let builder = Builder.FormatterBuilder.create ()
+        |> Builder.FormatterBuilder.with_indent_size indent_size
+        |> Builder.FormatterBuilder.with_max_line_length max_line_length in
+      Builder.FormatterBuilder.format_with_config builder lexbuf
+  | Some file -> 
+      (* Format from file *)
+      In_channel.with_file file ~f:(fun ic ->
+        let lexbuf = Lexing.from_channel ic in
+        let builder = Builder.FormatterBuilder.create ()
+          |> Builder.FormatterBuilder.with_indent_size indent_size
+          |> Builder.FormatterBuilder.with_max_line_length max_line_length in
+        Builder.FormatterBuilder.format_with_config builder lexbuf)
 ;;
 
 let command =
   Command.basic
-    ~summary:"File to format"
-    ~readme:(fun () -> "Path to .sql file that you want to format with pgformat")
-    (let%map_open.Command filedesc = anon (maybe ("filename" %: string)) in
-     fun () -> format_content filedesc)
+    ~summary:"Format SQL files with configurable options"
+    ~readme:(fun () -> "Path to .sql file that you want to format with pgformat. Use '-' or omit for stdin.")
+    (let%map_open.Command 
+       filedesc = anon (maybe ("filename" %: string))
+     and indent_size = flag "-indent-size" (optional_with_default 4 int) 
+         ~doc:"N Number of spaces for indentation (default: 4)"
+     and max_line_length = flag "-max-line-length" (optional_with_default 120 int)
+         ~doc:"N Maximum line length before wrapping (default: 120)"
+     in
+     fun () -> format_content ~indent_size ~max_line_length filedesc)
 ;;
 
-let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command
+let () = Command_unix.run ~version:"2.0" ~build_info:"pgformat with FormatterBuilder API" command
